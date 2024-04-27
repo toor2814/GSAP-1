a25a5966cb99da93c199db1a23ebfcd92691fe66
Muaaz ToOr <Toor2814@Gmail.com>
2024-04-27 12:47:18 +0500

    second commit real one

---
+++ script.js
@@ -0,0 +1,27 @@
+gsap.to("#page-2 h1",{
+    transform:"translateX(-120%)",
+    scrollTrigger:{
+        trigger:"#page-2",
+        scroller:"body",
+//        markers:true,
+        start:"top 0%",
+        end:"top -150%",
+        scrub:5,
+        pin:true,
+    }
+})
+
+
+// x:-1450,
+// duration:5,
+
+// gsap.to("#page2
+// t ransform: "t " ,
+// sc rot t Trigger:
+// trigger: "#page2" ,
+// scroller: "body",
+// ma rkers: true,
+// start: "top 0%",
+// end: "top —150%" ,
+// scrub:2,l
+// pin: true
