8f16fdf27c44f0fad8e924132fc7899186ca9fd8
Muaaz ToOr <Toor2814@Gmail.com>
2024-04-27 20:06:13 +0500

    uncompleted-commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var tl = gsap.timeline()
 tl.from("nav h2", {
     scale:0,
     duration:1,
-    scrub:5,
+    // scrub:5,
     delay:1,
     opacity:0
 })
@@ -33,6 +33,69 @@ gsap.to("#section-2 h1",{
         pin:true,
     }
 })
+
+let initialPath = `M 10 100 Q 450 100 890 100`
+let finalPath = `M 10 100 Q 450 100 890 100`
+
+var string = document.querySelector("#section-3")
+
+string.addEventListener("mousemove", function(dets){
+    initialPath = `M 10 80 Q ${dets.x} ${dets.y} 890 80`
+    gsap.to("svg path",{
+        attr:{d:initialPath}
+    })
+})
+
+string.addEventListener("mouseleave", function(dets){
+  gsap.to("svg path",{
+    attr:{d:finalPath},
+    duration:0.3,
+    ease: "elastic.out(1,0.3)",
+  })  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //        markers:true, 
 // x:-1450,
 // duration:5,
