6e92f4569c333d93f473d671d42a61d9418118dc
Muaaz ToOr <Toor2814@Gmail.com>
2024-04-27 14:51:18 +0500

    page-1 completed

--- script.js
+++ script.js
@@ -1,17 +1,69 @@
+var tl = gsap.timeline()
+
+tl.from("nav h2", {
+    scale:0,
+    duration:1,
+    scrub:5,
+    delay:1,
+    opacity:0
+})
+
+tl.from("#part-2 h4",{
+    y:-70,
+    duration:0.7,
+    stagger:0.2,
+})
+
+tl.from("#page-1 h1", {
+    y:20,
+    opacity:0,
+    scale:0.5,
+    duration:0.5,
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 gsap.to("#page-2 h1",{
     transform:"translateX(-120%)",
     scrollTrigger:{
         trigger:"#page-2",
         scroller:"body",
-//        markers:true,
         start:"top 0%",
         end:"top -150%",
-        scrub:5,
+        scrub:2,
         pin:true,
     }
 })
-
-
+//        markers:true, 
 // x:-1450,
 // duration:5,
 
